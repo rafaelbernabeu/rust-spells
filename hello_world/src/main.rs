f0171933e9cb5f57da08725d09e0c5461b3ae1db
Rafael Bernabeu <rbbernabeu@gmail.com>
2020-07-08 00:43:49 -0300

    update

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,15 +1,17 @@
 use std::io;
-use std::io::{Read, Write};
+use std::io::Write;
 
 fn main() {
     println!("Digite o seu nome:");
 
     let mut name: String = String::new();
 
-    io::stdin().read_line(&mut name);
+    io::stdin().read_line(&mut name).unwrap();
 
-    print!("Olá {}!", &name.trim());
+    println!("Olá {}!", &name.trim());
 
-    let name_encod = [10, 82, 97, 102, 97, 101, 108]; //UTF-8 as Decimal
-    io::stdout().write(&name_encod);
+    let name_encod = [82, 97, 102, 97, 101, 108, 10]; //UTF-8 as Decimal
+    let error = [69, 114, 114, 111]; //UTF-8 as Decimal
+    io::stdout().write(&name_encod).unwrap();
+    io::stderr().write(&error).unwrap();
 }
