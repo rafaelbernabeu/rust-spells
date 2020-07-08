645d87a7a440b73426e7edffeae95d03971b0076
Rafael Bernabeu <rbbernabeu@gmail.com>
2020-07-07 23:56:26 -0300

    read and write to stdin and stdout

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,12 +1,15 @@
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
 
 fn main() {
     println!("Digite o seu nome:");
 
-    let mut nome: String = String::new();
+    let mut name: String = String::new();
 
-    io::stdin().read_line(&mut nome);
+    io::stdin().read_line(&mut name);
 
-    print!("Olá {} !", &nome.trim());
+    print!("Olá {}!", &name.trim());
+
+    let name_encod = [10, 82, 97, 102, 97, 101, 108]; //UTF-8 as Decimal
+    io::stdout().write(&name_encod);
 }
