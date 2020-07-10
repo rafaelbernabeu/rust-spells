16f7cba2e107317a2a3f86306673b6726b333060
Rafael Bernabeu <rbbernabeu@gmail.com>
2020-07-09 22:44:34 -0300

    ordering methods

--- tcp_server/src/main.rs
+++ tcp_server/src/main.rs
@@ -1,13 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = String::new();
-    stream.write(String::from("Resposta do Server").as_bytes());
-    stream.read_to_string(&mut buffer);
-    println!("{:#?}", buffer);
-}
-
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:80")?;
 
@@ -16,4 +9,11 @@ fn main() -> std::io::Result<()> {
         handle_client(stream?);
     }
     Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = String::new();
+    stream.write(String::from("Resposta do Server").as_bytes());
+    stream.read_to_string(&mut buffer);
+    println!("{:#?}", buffer);
 }
